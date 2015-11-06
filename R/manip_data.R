8310581f08fdc302ecca819c020003ab2a342057
Andrew Landgraf <andland@gmail.com>
2015-11-05 19:19:13 -0500

    small updates to data manip

--- R/manip_data.R
+++ R/manip_data.R
@@ -7,16 +7,18 @@
 #' @param formula the formula with the response on the left hand side
 #'   and the covariates on the right hand side
 #' @param data data.frame
+#' @param ... optional, currently unused, arguments
 #'
 #' @return a character vector with one value per observation
 #' @export
-model.frame.libFM <- function(formula, data) {
+model.frame.libFM <- function(formula, data, ...) {
   # the independent variables should all be factors
+  # TODO: check data is a data.frame
   is.response = attr(terms(formula), "response")
   if (is.response) {
     response_name = attr(attr(terms(formula), "factors"), "dimnames")[[1]][1]
     if (any(colnames(data) == response_name)) {
-      out.string = paste0(data[, response_name])
+      out.string = paste0(data[[response_name]])
     } else {
       warning("Response variable not in dataset. Using constant 1 as response.")
       out.string = paste0(rep(1, nrow(data)))
@@ -29,14 +31,15 @@ model.frame.libFM <- function(formula, data) {
   vars = attr(attr(terms(formula), "factors"), "dimnames")[[2]]
   col.num = 0
   for (var in vars) {
-    out.string = paste0(out.string, " ", col.num + as.numeric(data[, var]) - 1, ":", 1)
-    col.num = col.num + nlevels(data[, var])
+    out.string = paste0(out.string, " ", col.num + as.numeric(data[[var]]) - 1, ":", 1)
+    col.num = col.num + nlevels(data[[var]])
   }
   return(out.string)
 }
 
 
 matrix.libFM <- function(matrix, y) {
+  # TODO: check matrix is a matrix
   if (!missing(y)) {
     out.string = paste0(y)
   } else {
@@ -47,22 +50,25 @@ matrix.libFM <- function(matrix, y) {
   for (c in 1:ncol(matrix)) {
     out.string = ifelse(
       matrix[, c] != 0,
-      paste0(out.string, " ", c, ":", 1),
+      paste0(out.string, " ", c, ":", matrix[, c]),
       out.string
     )
   }
   return(out.string)
 }
 
-libFM.groups <- function(data, formula) {
+libFM.groups <- function(formula, data) {
   # the independent variables should all be factors
+  # data should be a data.frame
+  # TODO: check data is a data.frame
   vars = attr(attr(terms(formula), "factors"), "dimnames")[[2]]
+  # TODO: give length of 1 to non-factors
   groups = rep(
     0:(length(vars) - 1),
     as.numeric(apply(
-      data[, vars],
+      data[[vars]],
       2,
-      function(x) length(unique(x))
+      function(x) nlevels(x)
     ))
   )
   return(groups)
