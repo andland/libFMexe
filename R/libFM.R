6cfd92fe9ef297bc79b6e840223780308e2ddce4
Andrew Landgraf <andland@gmail.com>
2015-11-07 15:57:59 -0500

    libFM takes matrices as well now. closes #3

--- R/libFM.R
+++ R/libFM.R
@@ -1,23 +1,8 @@
 #' libFM factorization machines
 #'
-#' @param train training data.frame
-#' @param test testing data.frame
-#' @param formula formula of covariates included. They must all be factors
-#' @param global_bias whether to include an overall/global bias term
-#' @param variable_bias whether to include variable main effects/biases
-#' @param dim dimension of the two-way interaction
-#' @param task classifcation or regression
-#' @param method learning method
-#' @param init_stdev standard deviation used for initialization
-#'  of 2-way factors
-#' @param regular length 3 vector of regularization parameters for
-#'  global bias, variable biases, and interactions, respectively. Used with
-#'  SGD and ALS
-#' @param learn_rate learning rate used for SGD and adaptive SGD
-#' @param validation validation data.frame used for adaptive SGD
-#' @param verbosity how much feedback to give
-#' @param iter number of iterations
-#' @param exe_loc location of libfm.exe executable (if not in the PATH)
+#' @param train training data.frame, matrix, or character vector
+#' @param test testing data.frame, matrix, or character vector
+#' @param ... other, unused, arguments
 #'
 #' @details See the libFM manual, \url{http://www.libfm.org/libfm-1.42.manual.pdf},
 #'  for details on the parameters.
@@ -50,18 +35,86 @@
 #' @return
 #' A vector of the predicted values/probabilities
 #' @export
-libFM <- function(train, test, formula, global_bias = TRUE, variable_bias = TRUE, dim = 8,
+libFM <- function(train, test, ...) {
+  UseMethod("libFM")
+}
+
+#' @describeIn libFM
+#'
+#' @param formula formula of covariates included
+#' @param validation validation data.frame, matrix, or character vector used for
+#'   adaptive SGD
+#'
+#' @export
+libFM.data.frame <- function(train, test, formula, validation, ...) {
+  train = model_frame_libFM(formula, train)
+  if (!inherits(test, "data.frame")) {
+    stop("train is a data.frame but test is not")
+  }
+  test = model_frame_libFM(formula, test)
+  if (!missing(validation)) {
+    if (!inherits(validation, "data.frame")) {
+      stop("train is a data.frame but validation is not")
+    }
+    validation = model_frame_libFM(formula, validation)
+  }
+
+  libFM.default(train, test, validation = validation, ...)
+}
+
+#' @describeIn libFM
+#'
+#' @param y_train,y_test,y_validation numeric vectors of responses when train,
+#'   test, and validation are matrices
+#'
+#' @export
+libFM.matrix <- function(train, test, y_train, y_test, validation, y_validation, ...) {
+  if (inherits(train, "matrix") & missing(y_train)) {
+    stop("y_train is missing")
+  }
+  if (!inherits(test, "matrix")) {
+    stop("train is a matrix but test is not")
+  }
+  train = matrix_libFM(train, y_train)
+  test = matrix_libFM(test, y_test)
+  if (!missing(validation)) {
+    if (missing(y_validation)) {
+      stop("validation argument present but y_validation is missing")
+    }
+    if (!inherits(validation, "matrix")) {
+      stop("validation is a matrix but test is not")
+    }
+    validation = matrix_libFM(validation, y_validation)
+  }
+
+  libFM.default(train, test, validation = validation, ...)
+}
+
+#' @describeIn libFM
+#'
+#' @param global_bias whether to include an overall/global bias term
+#' @param variable_bias whether to include variable main effects/biases
+#' @param dim dimension of the two-way interaction
+#' @param task classifcation or regression
+#' @param method learning method
+#' @param init_stdev standard deviation used for initialization
+#'  of 2-way factors
+#' @param regular length 3 vector of regularization parameters for
+#'  global bias, variable biases, and interactions, respectively. Used with
+#'  SGD and ALS
+#' @param learn_rate learning rate used for SGD and adaptive SGD
+#' @param verbosity how much feedback to give
+#' @param iter number of iterations
+#' @param exe_loc location of libfm.exe executable (if not in the PATH)
+#'
+#' @export
+libFM.default <- function(train, test, global_bias = TRUE, variable_bias = TRUE, dim = 8,
                 task = c("c", "r"), method = c("mcmc", "sgd", "als", "sgda"),
                 init_stdev = 0.1, regular = c(0, 0, 0), learn_rate = 0.1, validation,
-                verbosity = 0, iter = 100, exe_loc) {
+                verbosity = 0, iter = 100, exe_loc, ...) {
   method = match.arg(method)
   task = match.arg(task)
   if (missing(exe_loc)) {
-#     if (.Platform$OS.type == "windows") {
-#       libfm_exe = "libfm"
-#     } else {
-#       libfm_exe = "libFM"
-#     }
     libfm_exe = "libFM"
   } else {
     libfm_exe = paste0("\"", file.path(exe_loc, "libfm"), "\"")
@@ -88,11 +141,8 @@ libFM <- function(train, test, formula, global_bias = TRUE, variable_bias = TRUE
   testloc = paste0(tempfile(), "libFMtest.txt")
   outloc = paste0(tempfile(), "out.txt")
 
-  train_libFM = model_frame_libFM(formula, train)
-  test_libFM = model_frame_libFM(formula, test)
-
-  write.table(train_libFM, file = trainloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
-  write.table(test_libFM, file = testloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
+  write.table(train, file = trainloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
+  write.table(test, file = testloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
 
   command = paste0(libfm_exe,
                    " -task ", task,
@@ -115,8 +165,7 @@ libFM <- function(train, test, formula, global_bias = TRUE, variable_bias = TRUE
   if (method == "sgda") {
     if (!missing(validation)) {
       validloc = paste0(tempfile(), "libFMvalid.txt")
-      valid_libFM = model_frame_libFM(formula, validation)
-      write.table(valid_libFM, file = validloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
+      write.table(validation, file = validloc, col.names = FALSE, row.names = FALSE, quote = FALSE)
 
       command = paste0(command,
                        " -validation ", validloc)
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ R Wrapper for the libFM Executable
 
 This package provides a rough interface to [libFM](http://www.libfm.org/) using the executable with [R](https://www.r-project.org/). It does this in two ways:
 
--   The `model.frame.libFM()` function converts data into libFM (also LIBSVD) format
+-   `model_frame_libFM()` and `matrix_libFM()` convert data into libFM (also LIBSVD) format
 -   It calls the libFM executable with your data and returns the resulting prediction
 
 Installing
@@ -64,10 +64,10 @@ predFM = libFM(train, test, Rating ~ User + Movie,
                task = "r", dim = 10, iter = 500)
 
 mean((predFM - test$Rating)^2)
-#> [1] 0.8186918
+#> [1] 0.8192223
 ```
 
-This gives a mean squared error of 0.8186918 with dimension 10.
+This gives a mean squared error of 0.8192223 with dimension 10.
 
 We can compare to something simpler, such as ridge regression. Ridge regression cannot model interactions of users and movies because each interaction is observed at most once.
 
@@ -94,10 +94,10 @@ predFM_RR = libFM(train, test, Rating ~ User + Movie,
                   task = "r", dim = 0, iter = 100)
 
 mean((predFM_RR - test$Rating)^2)
-#> [1] 0.8882225
+#> [1] 0.8879214
 ```
 
-This gives a mean squared error of 0.8882225, nearly the same as ridge regression.
+This gives a mean squared error of 0.8879214, nearly the same as ridge regression.
 
 Improving this package
 ----------------------
